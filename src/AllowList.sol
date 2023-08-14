49c7e39e9861ecd8ece65ab6f7c80aad51c42943
Bruce Xu <bruce.xu@lxdao.io>
2023-08-14 15:07:54 +1200

    Add delegatable

--- src/AllowList.sol
+++ src/AllowList.sol
@@ -1,7 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.15;
 
-contract AllowList {
+import {Delegatable} from "lib/delegatable-sol/contracts/Delegatable.sol";
+import {DelegatableCore} from "lib/delegatable-sol/contracts/DelegatableCore.sol";
+
+// import "lib/OpenZeppelin/openzeppelin-contracts/access/Ownable.sol";
+
+contract AllowList is Delegatable {
+    constructor() Delegatable("AllowList", "1") {}
+
     mapping(address => bool) allowlist;
 
     function setAllowItem(address addr) public {
@@ -14,5 +21,27 @@ contract AllowList {
 
     function verify(address addr) public view returns (bool) {
         return allowlist[addr];
+    }
+
+    function _msgSender()
+        internal
+        view
+        virtual
+        override(DelegatableCore, Context)
+        returns (address sender)
+    {
+        if (msg.sender == address(this)) {
+            bytes memory array = msg.data;
+            uint256 index = msg.data.length;
+            assembly {
+                sender := and(
+                    mload(add(array, index)),
+                    0xffffffffffffffffffffffffffffffffffffffff
+                )
+            }
+        } else {
+            sender = msg.sender;
+        }
+        return sender;
     }
 }
